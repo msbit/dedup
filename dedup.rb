c4cb3ab0c51cf0dcd02dd0d327bdeb245d649e1b
Tom Sullivan <tom@msbit.com.au>
2018-11-07 14:46:35 +1100

    Formatting

--- dedup.rb
+++ dedup.rb
@@ -7,7 +7,7 @@ hashes = {}
 
 Dir.glob('**/*').select { |path| File.file?(path) }.each do |file|
   digest = Digest::SHA512.file(file).to_s
-  hashes[digest] = [] unless hashes.key? digest
+  hashes[digest] = [] unless hashes.key?(digest)
   hashes[digest] << file
 end
 
