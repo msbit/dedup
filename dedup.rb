822a83ce6222034c3ed325d6f2ee14e91eeb691f
Tom Sullivan <tom@msbit.com.au>
2018-11-07 14:46:00 +1100

    string type for keys

--- dedup.rb
+++ dedup.rb
@@ -6,7 +6,7 @@ require 'json'
 hashes = {}
 
 Dir.glob('**/*').select { |path| File.file?(path) }.each do |file|
-  digest = Digest::SHA512.file file
+  digest = Digest::SHA512.file(file).to_s
   hashes[digest] = [] unless hashes.key? digest
   hashes[digest] << file
 end
