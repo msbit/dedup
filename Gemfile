50d32d3df03069e7af441514e053ac5e9290d8c7
Tom Sullivan <tom@msbit.com.au>
2018-11-07 16:21:45 +1100

    Output a progress bar

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ source 'https://rubygems.org'
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
+gem 'ruby-progressbar'
+
 group :development do
   gem 'rubocop'
 end
--- dedup.rb
+++ dedup.rb
@@ -2,15 +2,25 @@
 
 require 'digest'
 require 'json'
+require 'ruby-progressbar'
 
 all = {}
 
-ARGV.map { |p| File.absolute_path(p) }.each do |base_path|
-  Dir.glob("#{base_path}/**/*").select { |p| File.file?(p) }.each do |file|
-    digest = Digest::SHA1.file(file).to_s
-    all[digest] = [] unless all.key?(digest)
-    all[digest] << file
-  end
+ARGV << '.' if ARGV.empty?
+
+files = ARGV.map { |p| File.absolute_path(p) }.map do |base_path|
+  Dir.glob("#{base_path}/**/*").select { |p| File.file?(p) }
+end
+files.flatten!
+
+progress_bar = ProgressBar.create
+progress_bar.total = files.size
+
+files.each_with_index do |file, _i|
+  digest = Digest::SHA1.file(file).to_s
+  all[digest] = [] unless all.key?(digest)
+  all[digest] << file
+  progress_bar.increment
 end
 
 duplicate = all.select { |_k, v| v.size > 1 }
