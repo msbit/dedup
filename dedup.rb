d4e02869e612fa976eb2b7ed5bff66c3291e82f2
Tom Sullivan <tom@msbit.com.au>
2018-11-07 15:44:44 +1100

    Output results in JSON

--- dedup.rb
+++ dedup.rb
@@ -3,14 +3,19 @@
 require 'digest'
 require 'json'
 
-hashes = {}
+all = {}
 
 ARGV.map { |p| File.absolute_path(p) }.each do |base_path|
   Dir.glob("#{base_path}/**/*").select { |p| File.file?(p) }.each do |file|
     digest = Digest::SHA1.file(file).to_s
-    hashes[digest] = [] unless hashes.key?(digest)
-    hashes[digest] << file
+    all[digest] = [] unless all.key?(digest)
+    all[digest] << file
   end
 end
 
-pp hashes
+duplicate = all.select { |_k, v| v.size > 1 }
+unique = all.select { |_k, v| v.size == 1 }
+
+File.open('dedup.all.json', 'w') { |f| f.write(JSON.pretty_generate(all)) }
+File.open('dedup.duplicate.json', 'w') { |f| f.write(JSON.pretty_generate(duplicate)) }
+File.open('dedup.unique.json', 'w') { |f| f.write(JSON.pretty_generate(unique)) }
